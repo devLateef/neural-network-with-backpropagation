c2282eddc5355fa79cd306cf625f7e1e38ea8ab4
devLateef <lateefolatayo@gmail.com>
2024-12-25 00:03:19 +0100

    Predicted with MNIST data with 98% accuracy

--- backpropagation/NeuralNetwork.py
+++ backpropagation/NeuralNetwork.py
@@ -2,46 +2,42 @@ import numpy as np
 
 class NeuralNetwork:
     def __init__(self, layers, alpha=0.1):
-        self.W = [] # initialize the list of weights matrices
-        self.layers = layers # store the network architecture
-        self.alpha = alpha # store learning rate
-        for i in np.arange(0, len(layers) - 2): # Loop from first layer index then stop before reaching last 2 layers
-            w = np.random.randn(layers[i] + 1, layers[i + 1] + 1) # Randomly initialize weight, add extra node for the bias
+        # initialize the list of weights matrices
+        self.W = []
+        self.layers = layers
+        self.alpha = alpha
+        # Loop from first layer index then stop before reaching last 2 layers
+        for i in np.arange(0, len(layers) - 2):
+            # Randomly initialize weight, add extra node for the bias
+            w = np.random.randn(layers[i] + 1, layers[i + 1] + 1)
             self.W.append(w / np.sqrt(layers[i])) # Normalize it
             # print(self.W)
         
-        # the last two layers are a special case where the input
-        # connections need a bias term but the output does not
+        # the last two layers are a special case where the input connections need a bias term but the output does not
         w = np.random.randn(layers[-2] + 1, layers[-1])
         # print(w)
         self.W.append(w / np.sqrt(layers[-2]))
         
     def __repr__(self):
-        # construct and return a string that represents the network
-        # architecture
+        # construct and return a string that represents the network architecture
         return "NeuralNetwork: {}".format(
             "-".join(str(l) for l in self.layers))
     
     def sigmoid(self, x):
-        # compute and return the sigmoid activation value for a
-        # given input value
+        # compute and return the sigmoid activation value for a given input value
         return 1.0 / (1 + np.exp(-x))
     
     def sigmoid_deriv(self, x):
-        # compute the derivative of the sigmoid function ASSUMING
-        # that ‘x‘ has already been passed through the ‘sigmoid‘
-        # function
+        # compute the derivative of the sigmoid function
         return x * (1 - x)
         
     def fit(self, X, y, epochs=1000, displayUpdate=100):
         # insert a column of 1’s as the last entry in the feature
-        # matrix -- this little trick allows us to treat the bias
-        # as a trainable parameter within the weight matrix
+        # matrix, this  allows a trainable parameter within the weight matrix
         X = np.c_[X, np.ones((X.shape[0]))]
         # loop over the desired number of epochs
         for epoch in np.arange(0, epochs):
-            # loop over each individual data point and train
-            # our network on it
+            # loop over each individual data point and train our network on it
             for (x, target) in zip(X, y):
                 self.fit_partial(x, target)
             # check to see if we should display a training update
@@ -51,51 +47,21 @@ class NeuralNetwork:
     
     def fit_partial(self, x, y):
         # construct our list of output activations for each layer
-        # as our data point flows through the network; the first
-        # activation is a special case -- it’s just the input
-        # feature vector itself
         A = [np.atleast_2d(x)]
-        # print("The Value of A", A)
-        # print("The first Lenght", len(A))
-        # print("The result of the dot product: ", A[0].dot(self.W[0]))
         
         # FEEDFORWARD:
         # loop over the layers in the network
         for layer in np.arange(0, len(self.W)):
-            # feedforward the activation at the current layer by
-            # taking the dot product between the activation and
-            # the weight matrix -- this is called the "net input"
-            # to the current layer
+            # feedforward the activation at the current layer by taking the dot product
+            # between the activation and the weight matrix
             net = A[layer].dot(self.W[layer])
-            # computing the "net output" is simply applying our
-            # nonlinear activation function to the net input
             out = self.sigmoid(net)
-            # once we have the net output, add it to our list of
-            # activations
             A.append(out)
             
         # BACKPROPAGATION
-        # the first phase of backpropagation is to compute the
-        # difference between our *prediction* (the final output
-        # activation in the activations list) and the true target
-        # value
-        # print('Value of A after Append: ', A)
         error = A[-1] - y
-        # from here, we need to apply the chain rule and build our
-        # list of deltas ‘D‘; the first entry in the deltas is
-        # simply the error of the output layer times the derivative
-        # of our activation function for the output value
         D = [error * self.sigmoid_deriv(A[-1])]
-        # once you understand the chain rule it becomes super easy
-        # to implement with a ‘for‘ loop -- simply loop over the
-        # layers in reverse order (ignoring the last two since we
-        # already have taken them into account)
         for layer in np.arange(len(A) - 2, 0, -1):
-            # the delta for the current layer is equal to the delta
-            # of the *previous layer* dotted with the weight matrix
-            # of the current layer, followed by multiplying the delta
-            # by the derivative of the nonlinear activation function
-            # for the activations of the current layer
             delta = D[-1].dot(self.W[layer].T)
             delta = delta * self.sigmoid_deriv(A[layer])
             # print('Delta Derivate Value: ', delta)
@@ -104,40 +70,30 @@ class NeuralNetwork:
         # since we looped over our layers in reverse order we need to
         # reverse the deltas
         D = D[::-1]
-        # WEIGHT UPDATE PHASE
+        
+        # WEIGHT UPDATE
         # loop over the layers
         for layer in np.arange(0, len(self.W)):
-            # update our weights by taking the dot product of the layer
-            # activations with their respective deltas, then multiplying
-            # this value by some small learning rate and adding to our
-            # weight matrix -- this is where the actual "learning" takes
-            # place
+            # update the weights by taking the dot product of the layer activations with their respective deltas,
+            # then multiplying this value by some small learning rate and adding to our weight matrix
             self.W[layer] += -self.alpha * A[layer].T.dot(D[layer])
     
     def predict(self, X, addBias=True):
-        # initialize the output prediction as the input features -- this
-        # value will be (forward) propagated through the network to
-        # obtain the final prediction
+        # initialize the output prediction as the input features 
         p = np.atleast_2d(X)
         # check to see if the bias column should be added
         if addBias:
-            # insert a column of 1’s as the last entry in the feature
-            # matrix (bias)
+            # insert a column of 1’s as the last entry in the feature matrix (bias)
             p = np.c_[p, np.ones((p.shape[0]))]
             # loop over our layers in the network
         for layer in np.arange(0, len(self.W)):
-            # computing the output prediction is as simple as taking
-            # the dot product between the current activation value ‘p‘
-            # and the weight matrix associated with the current layer,
-            # then passing this value through a nonlinear activation
-            # function
+            # computing the output prediction
             p = self.sigmoid(np.dot(p, self.W[layer]))
         # return the predicted value
         return p
     
     def calculate_loss(self, X, targets):
-        # make predictions for the input data points then compute
-        # the loss
+        # make predictions for the input data points then compute the loss
         targets = np.atleast_2d(targets)
         predictions = self.predict(X, addBias=False)
         loss = 0.5 * np.sum((predictions - targets) ** 2)
--- nn_mnist.py
+++ nn_mnist.py
@@ -1,13 +1,11 @@
-# import the necessary packages
 from backpropagation.NeuralNetwork import NeuralNetwork
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn import datasets
 
-# load the MNIST dataset and apply min/max scaling to scale the
-# pixel intensity values to the range [0, 1] (each image is
-# represented by an 8 x 8 = 64-dim feature vector)
+# load the MNIST dataset and apply min/max scaling to scale the pixel intensity values to the range [0, 1]
+# (each image is represented by an 8 x 8 = 64-dim feature vector)
 print("[INFO] loading MNIST (sample) dataset...")
 digits = datasets.load_digits()
 data = digits.data.astype("float")
@@ -15,24 +13,23 @@ data = (data - data.min()) / (data.max() - data.min())
 print("[INFO] samples: {}, dim: {}".format(data.shape[0], data.shape[1]))
 
 # construct the training and testing splits
-(trainX, testX, trainY, testY) = train_test_split(data, digits.target, test_size=0.25)
+(X_train, X_test, y_train, y_test) = train_test_split(data, digits.target, test_size=0.25)
 
 # convert the labels from integers to vectors
-trainY = LabelBinarizer().fit_transform(trainY)
-testY = LabelBinarizer().fit_transform(testY)
-print(trainY.shape, testY.shape)
+y_train = LabelBinarizer().fit_transform(y_train)
+y_test = LabelBinarizer().fit_transform(y_test)
 
 # train the network
 print("[INFO] training network...")
-nn = NeuralNetwork([trainX.shape[1], 32, 16, 10])
+nn = NeuralNetwork([X_train.shape[1], 32, 16, 10])
 print("[INFO] {}".format(nn))
-nn.fit(trainX, trainY, epochs=1000)
+nn.fit(X_train, y_train, epochs=1000)
 
 # evaluate the network
 print("[INFO] evaluating network...")
-predictions = nn.predict(testX)
+predictions = nn.predict(X_test)
 predictions = predictions.argmax(axis=1)
 print(predictions.shape)
-print(classification_report(testY.argmax(axis=1), predictions))
+print(classification_report(y_test.argmax(axis=1), predictions))
 
 
